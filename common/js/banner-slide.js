374462aff263989d49ab1c4b393e7a7d26582415
weiyang <503945930@qq.com>
2015-10-26 13:32:56 +0800

    1026 1.32

--- common/js/banner-slide.js
+++ common/js/banner-slide.js
@@ -1,11 +1,21 @@
   //menu
-$(document).ready(function(){
-  
-  $('li.mainlevel').mousemove(function(){
-  $(this).find('ul').slideDown("1000");//you can give it a speed
+  $(document).ready(function() {
+
+      $('li.mainlevel').mousemove(function() {
+          $(this).find('ul').slideDown("1000"); //you can give it a speed
+      });
+      $('li.mainlevel').mouseleave(function() {
+          $(this).find('ul').slideUp("fast");
+      });
+
+
+      $(".nav_a").mouseover(function() {
+          $(this).find(" > a").addClass("ahover");
+          $(".nav_xialab").delay(100).slideDown(100);
+      }).closest(".banner").mouseleave(function() {
+          $(".nav_xialab").delay(100).slideUp(100);
+          $(".nav_a").find(" > a").removeClass("ahover");
+
+      });
+
   });
-  $('li.mainlevel').mouseleave(function(){
-  $(this).find('ul').slideUp("fast");
-  });
-  
-});
